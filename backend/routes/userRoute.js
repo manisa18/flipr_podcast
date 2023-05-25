e4a01a171a85c195f0d8b87c78590608b84754f2
Manisa Basak <73214790+manisa18@users.noreply.github.com>
2023-05-25 21:16:41 +0530

    Renamed files

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,14 +1,9 @@
 const express = require("express");
 const {
-  registerUser,
-  loginUser,
-  logout,
-  googleAuth,
-} = require("../controllers/userController");
+} = require("../controllers/adminController");
 const router = express.Router();
 
-router.route("/register").post(registerUser);
-router.route("/login").post(loginUser);
-router.route("/logout").get(logout);
-router.post("/google", googleAuth);
+router.route("/").post(createPodcast);
+
+
 module.exports = router;
