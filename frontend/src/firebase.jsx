702e0c0e36d383df8cd5ce49aa9e90c8f62fb81e
Manisa Basak <73214790+manisa18@users.noreply.github.com>
2023-07-07 13:02:23 +0530

    Update firebase.jsx

--- frontend/src/firebase.jsx
+++ frontend/src/firebase.jsx
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyACbmQJf5VdeOp66pP7NXIR4tUiXxNHjyQ",
-  authDomain: "podcast-f4c97.firebaseapp.com",
-  projectId: "podcast-f4c97",
-  storageBucket: "podcast-f4c97.appspot.com",
-  messagingSenderId: "602876704726",
-  appId: "1:602876704726:web:6966dc61535ec189849677",
-  measurementId: "G-08JN2FN4CD",
+ 
 };
 
 const app = initializeApp(firebaseConfig);
